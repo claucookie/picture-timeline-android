d5964532c19abe6446a01bf311e37651c2f874c5
claucookie <claudia.luque.fernandez@gmail.com>
2020-02-12 11:10:58 +0000

    Add location permissions logic

--- app/src/main/java/dev/claucookielabs/picstimeline/presentation/MainActivity.kt
+++ app/src/main/java/dev/claucookielabs/picstimeline/presentation/MainActivity.kt
@@ -5,13 +5,17 @@ import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.multi.BaseMultiplePermissionsListener
+import com.karumi.dexter.listener.multi.CompositeMultiplePermissionsListener
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.multi.SnackbarOnAnyDeniedMultiplePermissionsListener
 import dev.claucookielabs.picstimeline.R
 import dev.claucookielabs.picstimeline.databinding.ActivityMainBinding
 import dev.claucookielabs.picstimeline.presentation.ui.ImagesAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.scope.currentScope
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -22,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupDataBinding()
-        startTracking()
+        checkLocationPermissions()
     }
 
     private fun setupDataBinding() {
@@ -38,22 +42,36 @@ class MainActivity : AppCompatActivity() {
     private fun startTracking() {
         mainViewModel.startTracking()
 
+    }
+
+    private fun checkLocationPermissions() {
+        val onPermissionsCheckedListener: MultiplePermissionsListener =
+            object : BaseMultiplePermissionsListener() {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report?.areAllPermissionsGranted() == true) startTracking()
+                }
+            }
+        val snackbarMultiplePermissionsListener: MultiplePermissionsListener =
+            SnackbarOnAnyDeniedMultiplePermissionsListener.Builder
+                .with(
+                    coordinator_view,
+                    "Location permission is required for this app to work. "
+                            + "You can give permission in the Settings of the app."
+                )
+                .withOpenSettingsButton("Settings")
+                .build()
+
         Dexter.withActivity(this)
             .withPermissions(
                 ACCESS_FINE_LOCATION,
                 ACCESS_COARSE_LOCATION
-            ).withListener(object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
-
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    permissions: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
-                    token: PermissionToken?
-                ) {
-
-                }
-            }).check();
+            ).withListener(
+                CompositeMultiplePermissionsListener(
+                    onPermissionsCheckedListener,
+                    snackbarMultiplePermissionsListener
+                )
+            )
+            .check()
     }
 
 }
